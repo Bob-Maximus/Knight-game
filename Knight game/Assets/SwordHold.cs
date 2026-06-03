f1b47c6b2fd729063662d3797edc682e320d39c9
Bob-Maximus <1elkabbanyou@hdsb.ca>
2026-06-02 21:20:15 -0400

    added sword and different block positions

---
+++ Knight game/Assets/SwordHold.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordHold : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public float rotationSpeed;
+    public Transform rotationGoal;
+    public Transform lookIndicator;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime*speed);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.deltaTime*rotationSpeed);
+    }
+
+/*
+    private Quaternion GetCameraLookDirection()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+        {
+            lookIndicator.position = Vector3.Lerp(lookIndicator.position, hit.point, Time.deltaTime*rotationSpeed);
+            rotationGoal.LookAt(lookIndicator);
+            if (lookIndicator != null)
+            {
+                lookIndicator.transform.position=hit.point;
+            }
+            return rotationGoal.rotation;
+        }
+
+        return Camera.main.transform.rotation;
+    }
+    */
+}
