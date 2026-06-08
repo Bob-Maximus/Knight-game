5d06160f2629ef050d16ecc026d06d50f5647833
Bob-Maximus <1elkabbanyou@hdsb.ca>
2026-06-08 12:04:01 -0400

    kinda fixed it?

--- Knight game/Assets/body/ActiveRagdollController.cs
+++ Knight game/Assets/body/ActiveRagdollController.cs
@@ -17,11 +17,11 @@ public class ActiveRagdollController : MonoBehaviour
     void Start()
     {
         //joints = new ConfigurableJoint[transforms.Count()-1];
-        initialRotations = new Quaternion[transforms.Count()-1];
+        initialRotations = new Quaternion[transforms.Count()];
 
         for (int i = 0; i<joints.Count(); i++)
         {
-            initialRotations[i] = transforms[i+1].rotation;
+            initialRotations[i] = transforms[i].rotation;
             //joints[i] = transforms[i+1].GetComponent<ConfigurableJoint>();
         }
     }
@@ -35,7 +35,8 @@ public class ActiveRagdollController : MonoBehaviour
                 joints[i].SetTargetRotationLocal(Quaternion.Euler(90, 0, 0), joints[i].transform.rotation);
                 continue;
             }
-            joints[i].SetTargetRotationLocal(transforms[i+1].transform.rotation, joints[i].transform.rotation);
+            //joints[i].SetTargetRotationLocal(transforms[i].transform.rotation, joints[i].transform.rotation);
+            ConfigurableJointExtensions.SetTargetRotationLocal(joints[i], transforms[i].transform.rotation, initialRotations[i]);
         } 
 
         if (balancing)
